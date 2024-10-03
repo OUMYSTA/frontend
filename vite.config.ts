620fa182729d0b02e782a09b51b01764cabf5240
OUMYSTA <oum1234thakhek@gmail.com>
2024-10-03 20:45:42 +0700

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
   plugins: [
     vue(),
   ],
